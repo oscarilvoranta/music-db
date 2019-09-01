47dc31394940ada2e7c73114778292cc3b10e6af
oscarilvoranta <oscar.ilvoranta@gmail.com>
2019-09-01 17:12:01 +0300

    delete and add works

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
+const bodyParser = require('body-parser');
 
 const sqlite3 = require('sqlite3').verbose()
 const DBSOURCE = "music.db"
@@ -19,45 +20,17 @@ app.get("/", (req, res) => {
 })
 
 app.use(express.static('public'))
-
-app.get("/throwers", (req, res) => {
-    var sql = `SELECT * FROM thrower`
-    var                                                                                                                                                                                           params = []
-    db.all(sql, params, (err, rows) => {
-        if(err){
-            res.status(400).json({"error":err.message});
-            return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-    })
-})
-
-app.get("/hole", (req, res) => {
-    var sql = `SELECT * FROM hole`
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if(err){
-            res.status(400).json({"error":err.message});
-            return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-    })
-})
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/hmm", (req, res) => {
-    var sql = `SELECT artist.name, song.songname
+    var sql = `SELECT artist.name, song.songname, artist.id, artist_song.id3, song.id2
     FROM artist
     LEFT JOIN artist_song
     ON artist.id = artist_song.artist_id
     LEFT JOIN song
-    ON artist_song.song_id = song.id`
+    ON artist_song.song_id = song.id2
+    WHERE songname IS NOT NULL`
     var params = []
     db.all(sql, params, (err, rows) => {
         if(err){
@@ -71,7 +44,57 @@ app.get("/hmm", (req, res) => {
     })
 })
 
+app.get("/delete/:id/:id2", (req, res, next) => {
+    var id = req.params.id
+    var id2 = req.params.id2
+    console.log(id)
+ 
+    db.run(`DELETE FROM artist_song WHERE id3=?`, id, function(err) {
+     if (err) {
+       return console.error(err.message);
+     }
+     db.run(`DELETE FROM song WHERE id2=?`, id2, function(err) {
+        if (err) {
+          return console.error(err.message);
+        }
+   
+      });
+   });
+ 
+    res.json({
+      "message":"deleted song "+id
+    })
+  })
+
 app.post("/add", function(req, res){
+    console.log(req.body)
+    var artist = req.body.artist;
+    var song = req.body.song;
+    var id1;
+    var id2;
+
+
+    db.run(`INSERT INTO artist (name) VALUES('${artist}')`, function(err){
+        if (err){
+            return console.log(err.message);
+        }
+        id1 = this.lastID;
+        console.log(`1 inserted rowId: ${this.lastID}`)
+        db.run(`INSERT INTO song (songname) VALUES('${song}')`, function(err){
+            if(err){
+                return console.log(err.message);
+            }
+            id2 = this.lastID;
+            console.log(`2 inserted rowId: ${this.lastID}`);
+            db.run(`INSERT INTO artist_song (artist_id, song_id) VALUES('${id1}', '${id2}')`, function(err){
+                if(err){
+                    return console.log(err.message);
+                }
+                console.log(`3 inserted rowId: ${this.lastID}`);
+                res.send('hej')
+            })
+        })
+    })
 
 })
 
